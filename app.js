26f24cb664aa9aa2e43a38d6c5976087adc7caca
FCeledon98 <f.celedon98@gmail.com>
2025-09-22 19:33:56 -0300

    Indicamos Objetivos/Funcionalidades

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Agregar nombres
+
+//Validar entrada
+
+//Visualizar la lista
+
+//Sorteo aleatorio
