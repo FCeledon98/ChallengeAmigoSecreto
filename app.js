a7d0842e379ace4cc2cd0b93541c7fb076d97508
f.celedon98@gmail.com <fa.celedoni@gmail.com>
2025-09-23 12:21:56 -0300

    Actualizar JavaScript, funciones

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Array para almacenar los nombres:
+let listaAmigos = [];
 
-//Agregar nombres
+// Funcion para agregar nombres de amigos
+function agregarAmigo() {
+  const input = document.getElementById("amigo"); //Capturar el valor del campo de entrada
+  const nombre = input.value.trim(); //eliminar espacios
+ 
+    //Validar que en campo no esté vacío:
+    if (nombre === ''){
+        alert('Por favor, ingrese un nombre.');
+        return;
+    }
 
-//Validar entrada
+    listaAmigos.push(nombre); //Actualizar Array 
+    input.value = ''; //Limpiar campo de entrada
+    actualizarListaAmigos();
+    input.focus();
+}
 
 //Visualizar la lista
+function actualizarListaAmigos() {
+    const listaAmigosElement = document.getElementById('listaAmigos');
+    listaAmigosElement.innerHTML = '';
+    
+    listaAmigos.forEach((amigo, index) => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigosElement.appendChild(li);
+    });
+}
 
 //Sorteo aleatorio
+ function sortearAmigo() {
+
+ }
